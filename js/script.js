4bd7780110a380d2fbf01e29b176b493f78b43fc
rcplaza <rcplaza.z@gmail.com>
2015-03-24 21:38:52 -0400

    Primer taser del portal Bienvenido Bebé

---
+++ js/script.js
@@ -0,0 +1,18 @@
+(function($){
+	$("#owl-demo").owlCarousel({
+	 
+	      autoPlay: 2000, //Set AutoPlay to 3 seconds
+	 
+	      items : 3,
+	      itemsDesktop : [1199,3],
+	      itemsDesktopSmall : [979,3]
+	 
+	  });
+
+
+
+    $("#slider").excoloSlider();
+
+})(jQuery);//end
+ 
+  
